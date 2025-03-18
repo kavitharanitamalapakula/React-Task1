e5210b94f43a6f818491e99c14d04b723000e8b5
kavitharanitamalapakula <kavitharaninissy@gmail.com>
2025-03-18 20:44:40 +0530

    align

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,8 +6,8 @@ export function Header() {
     return <>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
             <div class="container">
+                <img src={img} alt="Logo" width="40" height="40" class="d-inline-block align-text-top" />
                 <a class="navbar-brand" href="#">
-                    <img src={img} alt="Logo" width="40" height="40" class="d-inline-block align-text-top" />
                     MyWebsite
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
