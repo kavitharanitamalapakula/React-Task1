bd7a5ce830355d0846080b3d4b811569bc7989e1
kavitharanitamalapakula <kavitharaninissy@gmail.com>
2025-03-18 20:54:21 +0530

    toggle

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,13 +10,14 @@ export function Header() {
                 <a class="navbar-brand" href="#">
                     MyWebsite
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
+
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <Navbar />
-                    <li class="nav-item"><a class="btn btn-primary me-2" href="#">Sign In</a></li>
-                    <li class="nav-item"><a class="btn btn-primary" href="#">Sign Up</a></li>
+                    <li className="nav-item"><a className="btn btn-primary me-2" href="#">Sign In</a></li>
+                    <li className="nav-item"><a className="btn btn-primary" href="#">Sign Up</a></li>
                 </div>
             </div>
         </nav>
